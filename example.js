28e114fafa19140e1cffa5a19ac27649f8feef2e
draeder <draeder@gmail.com>
2020-10-17 03:18:12 -0500

    Update example.js

--- example.js
+++ example.js
@@ -7,46 +7,35 @@ let bought = false
 
 sho.on("result", result => {
     //console.log("SHO:",result)
-    if(result >= 0) {
+    if(result < -60 || result < -40 || result == 0) {
         if(bought == false){
-            console.log("BUY: Uptrend", "Price: ", array.slice(-1)[0])
+            console.log("BUY - Oversold:", result, "Price:", array.slice(-1)[0])
             bought = true
         }
     }
-    if(result < -40) {
-        if(bought == false){
-            console.log("BUY: Oversold", "Price: ", array.slice(-1)[0])
-            bought = true
-        }
-    }
-    if(result < -60) {
-        if(bought == false){
-            console.log("BUY: Etremely oversold", "Price: ", array.slice(-1)[0])
-            bought = true
-        }
-    }
-    if(result > 40) {
+    
+    if(result > 60 || result > 40) {
         if(bought == true){
-            console.log("SELL: Overbought", "Price: ", array.slice(-1)[0])
-            bought = false
-        }
-    }
-    if(result > 60) {
-        if(bought == true){
-            console.log("SELL: Etremely overbought", "Price: ", array.slice(-1)[0])
+            console.log("SELL - Overbought:", result, "Price:", array.slice(-1)[0])
             bought = false
         }
     }
 })
 
 //** random example data
-let min = 2.5
-let max = 60
+let base = 451 // base price
+let percent = 7 // price+- percent swing
+
+let range = (percent / 100) * base // range percent
+let min = base-range/2
+let max = base+range/2
 
 setInterval(()=>{
     array.push(rand(min, max)) // simulates stock price fluctuations
-    sho.get("result", {array}) // send data to get real time SHO
-},10) // simulates the random nature of market data change intervals
+    sho.get("result", {array}) // send data to get realtime SHO
+},
+    1 // rand(10, 1000) simulates the random nature of market price change intervals
+) 
 
 function rand(min, max){
     let result = Math.random() * (max - min) + min
